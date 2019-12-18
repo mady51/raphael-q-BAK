3a54504a1c86a1056ef604b5d13d04a66b5e5a04
Demon000 <demonsingur@gmail.com>
2019-12-18 10:53:42 +0100

    msm: camera: import more Xiaomi changes
    Change-Id: I1ce1ab93a43beadef4281cec8a31f5f55ceaa42e

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.c
@@ -678,6 +678,31 @@ static int __cam_isp_ctx_handle_buf_done_in_activated_state(
 				next_req->request_id, ctx_isp->active_req_cnt, ctx->ctx_id);
 		}
 	}
+//	ctx_isp->irq_delay_detect = false;
+	if (ctx_isp->active_req_cnt > 0) {
+		if (list_empty(&ctx->active_req_list)) {
+			CAM_DBG(CAM_ISP, "Buf done with no active request!");
+			goto end;
+		}
+		next_req = list_first_entry(&ctx->active_req_list, struct cam_ctx_request, list);
+		CAM_DBG(CAM_REQ, "next_req:%p, active_reqcnt:%d", next_req, ctx_isp->active_req_cnt);
+
+		next_req_isp = (struct cam_isp_ctx_req *) next_req->req_priv;
+
+		CAM_DBG(CAM_REQ, "next_req_isp:bubble_detected:%d,bubble_report:%d",
+			next_req_isp->bubble_detected, next_req_isp->bubble_report);
+		if (next_req_isp->bubble_detected && next_req_isp->bubble_report) {
+			next_req_isp->num_acked = 0;
+			next_req_isp->bubble_detected = false;
+			list_del_init(&next_req->list);
+			list_add(&next_req->list, &ctx->pending_req_list);
+			atomic_set(&ctx_isp->process_bubble, 0);
+			ctx_isp->active_req_cnt--;
+			CAM_DBG(CAM_REQ,
+				"Move active request %lld to pending list(cnt = %d) [bubble recovery], ctx %u",
+				next_req->request_id, ctx_isp->active_req_cnt, ctx->ctx_id);
+		}
+	}
 end:
 	__cam_isp_ctx_update_state_monitor_array(ctx_isp,
 		CAM_ISP_STATE_CHANGE_TRIGGER_DONE,
