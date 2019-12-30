9a5f941ef54248eb9ec1ef5f7d71ca004a430c64
mady51 <root@instance-1.us-central1-a.c.reflected-coder-262904.internal>
2019-12-30 04:41:49 +0000

    Revert "Introducing KLapse - A kernel level livedisplay module v4.0:"
    This reverts commit e165decfd7fb38bdccbbcfa7fad744368f0c5719.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -43,16 +43,6 @@
 #include "dsi_panel_mi.h"
 
 
-#include <linux/string.h>
-#include "dsi_drm.h"
-#include "sde_crtc.h"
-#include "sde_rm.h"
-#include "sde_trace.h"
-
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-#endif
-
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -845,10 +835,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		pr_err("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
 	}
-	
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 
 	if ((panel->last_bl_lvl == 0 || (panel->skip_dimmingon == STATE_DIM_RESTORE)) && bl_lvl) {
 		if (panel->panel_on_dimming_delay)
